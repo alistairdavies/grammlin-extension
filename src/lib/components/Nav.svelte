fa06747d087615b3713e484c09dc25bdbb70f848
Alistair Davies <ali17sam@gmail.com>
2025-12-07 23:03:30 +0000

    Simplify nav styling

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+    import {  Settings} from "@lucide/svelte";
+
 </script>
 
-<div class="navbar bg-base-300">
-  <button class="btn btn-ghost text-xl">Grammlin</button>
-</div>
+<nav class="p-4">
+  <div class="flex justify-between items-center mb-2">
+    <p class="text-lg">Grammlin</p>
+    <button class="btn btn-xs">
+      <Settings size={16} /> Settings
+    </button>
+  </div>
+  <hr/>
+</nav>
